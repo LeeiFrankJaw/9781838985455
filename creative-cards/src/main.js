58ce322dc10a6d92c6581dfcec7a44b770f7e3f4
Lei Zhao <LeiZh26@gmail.com>
2020-05-04 14:44:34 +0800

    Global components

--- creative-cards/src/main.js
+++ creative-cards/src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.component('text-input', {
+  template: '<textarea :placeholder="textValue" @click="changeText"></textarea>',
+  data: () => ({textValue: 'Type here'}),
+  methods: {
+    changeText: function() {
+      this.textValue = 'Text changed'
+    }
+  }
+})
+
 new Vue({
   el: '#app',
-  render: h => h(App)
 })
