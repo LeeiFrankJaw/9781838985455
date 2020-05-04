f07c7ed93cf45bbb1e1092a6e9908538f4b469b6
Lei Zhao <LeiZh26@gmail.com>
2020-05-04 15:29:10 +0800

    Single file components

--- creative-cards/src/main.js
+++ creative-cards/src/main.js
@@ -1,18 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-var component1 = {
-  template: '<p>Component 1</p>'
-}
-
-var component2 = {
-  template: '<p>Component 2</p>'
-}
 
 new Vue({
   el: '#app',
-  components: {
-    'component-1': component1,
-    'component-2': component2
-  }
+  render: h => h(App)
 })
