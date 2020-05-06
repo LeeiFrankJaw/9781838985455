ce2b7d8f384179d07b1e49e13a957e54e43465fc
Lei Zhao <LeiZh26@gmail.com>
2020-05-06 16:12:42 +0800

    Text input component

---
+++ creative-cards/src/components/card/TextInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="row">
+    <div class="col-sm-12">
+      <div class="form-group">
+        <h4>Edit Text:</h4>
+        <textarea cols="50" rows="4"
+                  class="form-control"
+                  placeholder="Add text here!"
+                  v-model="textBoxInput"
+                  @keyup="textChanged">
+        </textarea>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      textBoxInput: ""
+    }
+  },
+  methods: {
+    textChanged() {
+      this.$emit('displayTextChanged', this.textBoxInput)
+    }
+  }
+}
+</script>
